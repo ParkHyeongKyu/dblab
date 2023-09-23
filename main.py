b66419a0660fabcc4525c3b75b648180259d417c
HyeongKyu Park <hyeongkyu.park@bagelcode.com>
2023-09-23 17:45:22 +0900

    apply jaehyun's example

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import numpy as np
 import json
 import argparse
 import importlib
+import time
 
 from adbench.run import RunPipeline
 from adbench.myutils import Utils
@@ -12,7 +13,7 @@ from sklearn.model_selection import train_test_split
 
 # 아래 download_datasets를 comment in 해서 최초 1번 실행시켜주어야 함
 # adbench library에 github에서 제공하는 default dataset을 Inject해야 RunPipeline이 실행 됨.
-# utils = Utils()
+utils = Utils()
 # utils.download_datasets(repo='github')
 
 config = {
@@ -32,6 +33,7 @@ import_path = model_import_paths.get(model_name)
 if import_path:
     module = importlib.import_module(import_path)
     model_class = getattr(module, model_name)
+    model = model_class(seed=42)
 else:
     raise ValueError(f'Invalid model name: {model_name}')
 
@@ -72,9 +74,20 @@ try:
     y = dataset['y']
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
 
-    model = model_class(seed=42)
-    model.fit(X_train, y_train)
-    score = model.predict_score(X_test)
+    # fitting
+    start_time = time.time()
+    model.fit(X_train=data['X_train'], y_train=data['y_train'])
+    end_time = time.time();
+    time_fit = end_time - start_time
+    print('Fit time: ' + str(time_fit))
+
+    # prediction
+    score = model.predict_score(data['X_test'])
+
+    # evaluation
+    result = utils.metric(y_true=data['y_test'], y_score=score, pos_label=1)
+
+    print(f"AUC-ROC: {result['aucroc']}, AUC-PR: {result['aucpr']}")
 
 except FileNotFoundError as err:
     print(f"Error: {err}")
